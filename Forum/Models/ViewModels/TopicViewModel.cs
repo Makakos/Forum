e545026d7afdb88b70663411bc16717f45df6c84
Maks <maximgaber65@gmail.com>
2021-02-22 17:06:33 +0200

    fixed the display of the last comment in discussions

--- Forum/Models/ViewModels/TopicViewModel.cs
+++ Forum/Models/ViewModels/TopicViewModel.cs
@@ -8,24 +8,26 @@ namespace Forum.Models.ViewModels
 {
     public class TopicViewModel
     {
-        
-        public List<Discussion> discussions { get; set; }
-        public List<Message> lastMessages { get; set; }
+        public Dictionary<Discussion,Message> discussAndLastMess { get; set; }
         public Topic Topic{get;set;}
 
         
         public TopicViewModel(DataManager dataManager,int id)
         {
             Topic = dataManager.topicsRepository.GetTopicById(id);
-            discussions = dataManager.topicsRepository.GetTopicById(id).Discussions;
-            lastMessages = new List<Message>();
+            var discussions = dataManager.topicsRepository.GetTopicById(id).Discussions;
             
+            discussAndLastMess = new Dictionary<Discussion, Message>();
 
             foreach(Discussion discussion in discussions)
             {
-                 lastMessages.Add(dataManager.discussionsRepository.GetDiscussionById(discussion.Id).Messages.
-                                     OrderByDescending(x => x.Date).FirstOrDefault());
-                 
+                 Message temp=dataManager.discussionsRepository.GetDiscussionById(discussion.Id)
+                    .Messages.LastOrDefault();
+
+                if(temp!=null)
+                    discussAndLastMess.Add(discussion, temp);
+                else
+                    discussAndLastMess.Add(discussion, new Message());
             }
           
         }
